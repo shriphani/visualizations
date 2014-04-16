3d28b8cb13368a17586aac0876f2f11f42601f1d
Shriphani Palakodety <shriphanip@gmail.com>
2014-04-16 00:32:03 -0400

    initial commit of arms sale vis

---
+++ arms_sale_visualization/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to arms_sale_visualization
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ arms_sale_visualization/test/arms_sale_visualization/core_test.clj
@@ -0,0 +1,7 @@
+(ns arms-sale-visualization.core-test
+  (:require [clojure.test :refer :all]
+            [arms-sale-visualization.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
