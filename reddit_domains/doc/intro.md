d78cfac5e71d0e3d022a5168f88f8423ead8b5df
Shriphani Palakodety <shriphanip@gmail.com>
2014-04-17 02:13:03 -0400

    added reddit domains project

---
+++ reddit_domains/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to reddit_domains
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ reddit_domains/test/reddit_domains/core_test.clj
@@ -0,0 +1,7 @@
+(ns reddit-domains.core-test
+  (:require [clojure.test :refer :all]
+            [reddit-domains.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
