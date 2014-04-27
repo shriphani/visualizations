ebf86145b0240e428632327125547d83c63cd6f3
shriphani <spalakod@cs.cmu.edu>
2014-04-27 00:47:34 -0400

    added more stuff

--- reddit_domains/src/reddit_domains/analyze.clj
+++ reddit_domains/src/reddit_domains/analyze.clj
@@ -1,6 +1,6 @@
 (ns reddit-domains.analyze
   (:require [clojure.java.io :as io])
-  (:use [incanter core stats datasets])
+  (:use [incanter core stats datasets charts])
   (:import [java.io PushbackReader]))
 
 (defn read-corpus-file
@@ -35,3 +35,19 @@
    {}
    (read-corpus-file corpus-file)))
 
+(defn measure-submissions
+  []
+  (map
+   (fn [i]
+     (let [corpus-name (str i ".corpus")]
+       [(+ 2006 i) (-> corpus-name
+                       read-corpus-file
+                       count)]))
+   (range 9)))
+
+(defn plot-submissions
+  []
+  (let [submissions-table (measure-submissions)
+        years (map first submissions-table)
+        subs  (map second submissions-table)]
+    ))
