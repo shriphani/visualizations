eefccc53506e2e38036d231d8fc7e89a0b2db8b3
Shriphani Palakodety <shriphanip@gmail.com>
2014-05-25 17:43:08 -0400

    added djia repo

---
+++ djia/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to djia
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ djia/test/djia/core_test.clj
@@ -0,0 +1,7 @@
+(ns djia.core-test
+  (:require [clojure.test :refer :all]
+            [djia.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
