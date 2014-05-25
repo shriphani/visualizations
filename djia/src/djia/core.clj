0c40a0d3edb6a42e450ad698734773348d7800a8
Shriphani Palakodety <shriphanip@gmail.com>
2014-05-25 18:22:56 -0400

    can get tables

--- djia/src/djia/core.clj
+++ djia/src/djia/core.clj
@@ -1,6 +1,30 @@
-(ns djia.core)
+(ns djia.core
+  (:require [clj-http.client :as client]
+            [net.cgrand.enlive-html :as html]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def wikipedia-djia-url "http://en.wikipedia.org/wiki/Historical_components_of_the_Dow_Jones_Industrial_Average")
+
+(defn download-tables
+  "Downloads tables and spits out nice things"
+  []
+  (let [body (-> wikipedia-djia-url
+                 client/get
+                 :body)
+        processed-body (-> body
+                           java.io.StringReader.
+                           html/html-resource)
+        dates  (filter
+                (fn [t]
+                  (re-find #"\d+" t))
+                (map
+                 html/text
+                 (html/select processed-body
+                              [:span.mw-headline])))
+        tables (map
+                (fn [table]
+                  (map
+                   html/text
+                   (html/select table [:td])))
+                (html/select processed-body
+                             [:table.wikitable]))]
+    (map vector dates tables)))
